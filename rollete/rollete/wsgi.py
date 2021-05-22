af2957103042536325f5bc378237cb3b797a50a2
Nikhilesh <nikhilesh.debbarma062@gmail.com>
2021-05-22 18:31:26 +0530

    mamamamdoddodo

--- rollete/rollete/wsgi.py
+++ rollete/rollete/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
-
+import sys
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +14,9 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rollete.settings')
 
 application = get_wsgi_application()
+
+
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+if path not in sys.path:
+    sys.path.append(path)
